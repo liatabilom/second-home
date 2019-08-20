c773cc3f6b316eb338098e2979af65c565506c2d
Lia <lia.tabilo@gmail.com>
2019-08-20 16:07:20 +0100

    Initial commit
    app moved from a old repository

---
+++ src/Card2.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Mobile from "./images/Mobile.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import "./Card2.css";
+
+class Card2 extends Component {
+  render() {
+    return (
+      <div>
+        <img className="mobile" src={Mobile} alt="" />
+        <FontAwesomeIcon icon={faPlusCircle} size="1x" className="plusIcon" />
+      </div>
+    );
+  }
+}
+
+export default Card2;
