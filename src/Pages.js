106faff8d568fd6de54fc4223d5bd28994a2cc02
Lia <lia.tabilo@gmail.com>
2020-02-29 17:11:54 +0000

    Change active by page

--- src/Pages.js
+++ src/Pages.js
@@ -5,10 +5,10 @@ class Pages extends Component {
 	render() {
 		return (
 			<div className="bottom-pages">
-				<a className="page-button active" href="https://www.correiagoncalves.com">
+				<a className="page-button" href="https://www.correiagoncalves.com">
 					Page 1
 				</a>
-				<a className="page-button" href="https://home2.correiagoncalves.com">
+				<a className="page-button active" href="https://home2.correiagoncalves.com">
 					Page 2
 				</a>
 				<a className="page-button" href="https://home3.correiagoncalves.com/">
